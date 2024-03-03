2096cced4580000eaaeeff4d1a106a8861be5198
Olav007 <git@orgt.eu>
2024-03-03 10:56:42 +0100

    baldr snapshot

---
+++ include/TemperatureControl.h
@@ -0,0 +1,41 @@
+#pragma once
+#include "TemperatureRegulationInterface.h"
+
+class TemperatureControl
+{
+private:
+    const TemperatureRegulationInterface& temperatureRegulationInterface;
+    float minTemp;
+    float maxTemp;
+public:
+
+    TemperatureControl(const TemperatureRegulationInterface& temperatureRegulationInterface, float minTemp, float maxTemp)
+		: temperatureRegulationInterface(temperatureRegulationInterface), minTemp(minTemp), maxTemp(maxTemp)
+	{
+	}
+    TemperatureControl(float minTemp, float maxTemp)
+    {
+        this->minTemp = minTemp;
+        this->maxTemp = maxTemp;
+    }
+
+    float getMinTemp() const
+    {
+        return minTemp;
+    }
+
+    void setMinTemp(float minTemp)
+    {
+        this->minTemp = minTemp;
+    }
+
+    float getMaxTemp() const
+    {
+        return maxTemp;
+    }
+
+    void setMaxTemp(float maxTemp)
+    {
+        this->maxTemp = maxTemp;
+    }
+};
---
+++ src/TemperatureControl.cpp
@@ -0,0 +1,1 @@
+#include "TemperatureControl.h"
