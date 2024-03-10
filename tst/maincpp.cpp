964313edb9fbfb4eaafd9b3e77f790847bd22d69
Olav007 <git@orgt.eu>
2024-03-10 15:41:30 +0100

    More tests

--- tst/maincpp.cpp
+++ tst/maincpp.cpp
@@ -2,7 +2,6 @@
 #include <gtest/gtest.h>
 #include <gmock/gmock.h>
 
-
 using ::testing::AtLeast;
 using ::testing::_;
 using ::testing::Return;
@@ -29,6 +28,7 @@ TEST(TemperatureControl, test_Heat_and_Interface_template)
     tc.regulateTemperature(18);
 }
 
+
 TEST(TemperatureControl, test_Exception)
 {
     TemperatureRegulationMock mocc;
@@ -44,18 +44,18 @@ TEST(TemperatureControl, test_Exception)
 }
 
 
-TEST(TemperatureControl, test_Change_temperature)
+TEST(TemperatureControl, test_Change_temperature_and_Cooling)
 {
     TemperatureRegulationMock mocc;
-    TemperatureControl<> tc(&mocc, 19, 21);
-    ASSERT_EQ(tc.getMinTemp(), 19);
-    ASSERT_EQ(tc.getMaxTemp(), 21);
+    TemperatureControl<> tc(&mocc, 19.010203f, 21.21f);
+    ASSERT_EQ(tc.getMinTemp(), 19.010203f);
+    ASSERT_EQ(tc.getMaxTemp(), 21.21f);
+    tc.setMinTemp(18.18f);
+    tc.setMaxTemp(23.23f);
     EXPECT_CALL(mocc, turnOnCooling());
-    tc.regulateTemperature(22);
-    tc.setMaxTemp(23);
-    tc.setMinTemp(18);
-    ASSERT_EQ(tc.getMinTemp(), 18);
-    ASSERT_EQ(tc.getMaxTemp(), 23);
+    tc.regulateTemperature(23.25);
+    ASSERT_EQ(tc.getMinTemp(), 18.18f);
+    ASSERT_EQ(tc.getMaxTemp(), 23.23f);
     bool exceptionThrown = false;
     try {
         tc.setMinTemp(24);
@@ -64,11 +64,54 @@ TEST(TemperatureControl, test_Change_temperature)
         exceptionThrown = true;
     }
     ASSERT_TRUE(exceptionThrown);
-
 }
 
 
+TEST(TemperatureControl, test_Heating_and_double)
+{
+    TemperatureRegulationMock mocc;
+    TemperatureControl<TemperatureRegulationInterfaceP, double> tc(&mocc, 19.01d, 21.21);
+    ASSERT_EQ(tc.getMinTemp(), 19.01d);
+    ASSERT_EQ(tc.getMaxTemp(), 21.21);
+    tc.setMinTemp(18.18);
+    tc.setMaxTemp(23.23);
+    EXPECT_CALL(mocc, turnOnHeating());
+    tc.regulateTemperature(18);
+    ASSERT_EQ(tc.getMinTemp(), 18.18);
+    ASSERT_EQ(tc.getMaxTemp(), 23.23);
+    bool exceptionThrown = false;
+    try {
+        tc.setMinTemp(24);
+    }
+    catch (...) { // Catches any exception
+        exceptionThrown = true;
+    }
+    ASSERT_TRUE(exceptionThrown);
+}
 
+TEST(TemperatureControl, test_Heating_TurnOff_and_int)
+{
+	TemperatureRegulationMock mocc;
+	TemperatureControl<TemperatureRegulationInterfaceP, int> tc(&mocc, 19.01f, 21.99f);
+	ASSERT_EQ(tc.getMinTemp(), 19);
+	ASSERT_EQ(tc.getMaxTemp(), 21);
+	tc.setMinTemp(18.18f);
+	tc.setMaxTemp(23.23f);
+	EXPECT_CALL(mocc, turnOnHeating());
+	tc.regulateTemperature(17);
+    EXPECT_CALL(mocc, turnOff());
+    tc.regulateTemperature(20);
+	ASSERT_EQ(tc.getMinTemp(), 18);
+	ASSERT_EQ(tc.getMaxTemp(), 23);
+	bool exceptionThrown = false;
+	try {
+		tc.setMinTemp(24);
+	}
+	catch (...) { // Catches any exception
+		exceptionThrown = true;
+	}
+	ASSERT_TRUE(exceptionThrown);
+}
 
 int main(int argc, char** argv) {
     ::testing::InitGoogleTest(&argc, argv);
